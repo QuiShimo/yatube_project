9729f9354e0e63ca4fc865e7fb3c76fdb968aa0f
Mikhail Bondarenko <quishimo404@ya.ru>
2022-10-09 15:04:50 +0900

    Подключен CSS

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('posts/index.html')
-    return HttpResponse(template.render({}, request))
+    template = 'posts/index.html'
+    context = {
+        'title': 'Yatube - главная страница',
+        'text': 'Это главная страница проекта Yatube',
+    }
+
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты группы {slug}')
+    template = 'posts/group_list.html'
+    context = {
+        'title': 'Yatube - информация о группах',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+
+    return render(request, template, context)
